bf919291ef4794affa0a3c184c8cdd0adca2e026
JoseCalzado <56232554+JoseCalzado@users.noreply.github.com>
2019-10-21 18:54:39 +0200

    Segundo intento

--- Code/src/Tester.java
+++ Code/src/Tester.java
@@ -3,7 +3,7 @@ public class Tester {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("\n Deja o no deja");
+		System.out.println("\n Soy el mejor");
 	}
 
 }
