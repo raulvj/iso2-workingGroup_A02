f238d483857b5b749ea3aead9e6460b53619cb43
aharo <aharo@LAPTOP-1G22HUN5>
2019-10-21 19:00:52 +0200

    que no me iba

--- Code/src/Tester.java
+++ Code/src/Tester.java
@@ -4,6 +4,7 @@ public class Tester {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("\n Soy el mejor");
+		System.out.println("\n Coincido con usted");
 	}
 
 }
