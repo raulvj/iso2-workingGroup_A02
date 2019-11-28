e403054b47fa7a67831e771b79ad36b6f9d59703
raulvj <33183247+raulvj@users.noreply.github.com>
2019-11-28 23:43:33 +0100

    Implementation of it5

---
+++ Code/src/it5/Supplyment.java
@@ -0,0 +1,25 @@
+package it5;
+
+public class Supplyment {
+
+	public void updateStock() {
+		// TODO - implement Supplyment.updateStock
+		throw new UnsupportedOperationException();
+	}
+
+	public void forestallCarParts() {
+		// TODO - implement Supplyment.forestallCarParts
+		throw new UnsupportedOperationException();
+	}
+
+	public void supplyWarehouse() {
+		// TODO - implement Supplyment.supplyWarehouse
+		throw new UnsupportedOperationException();
+	}
+
+	public void carPartsControl() {
+		// TODO - implement Supplyment.carPartsControl
+		throw new UnsupportedOperationException();
+	}
+
+}
